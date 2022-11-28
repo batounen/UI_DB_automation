638686c794183ef89d11909ec0aacb83e1589fbd
Bato Unen <batounen@gmail.com>
2022-11-28 12:00:45 -0700

    removed unused import statement

--- src/test/java/com/example/pages/Books_student.java
+++ src/test/java/com/example/pages/Books_student.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
