3d8e592febef1142e7ecf1ea34f359d9bff28f3f
Bato <batounen@gmail.com>
2022-12-10 11:07:01 -0700

    tag updated to @smoke

--- src/test/java/com/example/runner/CukesRunner.java
+++ src/test/java/com/example/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/example/step_definitions",
-        tags = "@us1",
+        tags = "@smoke",
         publish = true
 )
 
